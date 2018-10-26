1b4e6143104ceabb2aaaba5dd739937c85b58a1e
Adiel Matuf <matufadiel@gmail.com>
2018-10-26 12:36:36 +0300

    Add camera permmision

--- app/src/main/java/com/example/adiel/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/adiel/cameraapp/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.adiel.cameraapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,17 +24,28 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    static final int REQUEST_TAKE_PHOTO = 1;
-    String mCurrentPhotoPath;
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
+
+    //GUI object
     String name;
     Button updateName;
     EditText nameField;
+
+    //Camera
+    FloatingActionButton btnCamera;
+    Activity activity;
+    ImageView ivimage;
+
+    public Uri fileUri;
+    private int CAMERA_REQUEST = 100;
+    private int Gallary_REQUEST = 101;
 
     TextView welcomTitle;
 
@@ -34,14 +53,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        activity = MainActivity.this;
+        Camerapermission();
         updateName = (Button)findViewById(R.id.updateName);
         nameField = (EditText)findViewById(R.id.nameField);
 
         welcomTitle = (TextView)findViewById(R.id.welcomTitle);
         welcomTitle.setText("welcom!");
 
-        FloatingActionButton btnCamera = (FloatingActionButton)findViewById(R.id.btnCamera);
+        btnCamera = (FloatingActionButton)findViewById(R.id.btnCamera);
+        ivimage = (ImageView) findViewById(R.id.imageView);
 
         name = "";
 
@@ -54,11 +75,132 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
+                openCamera();
             }
         });
+    }
+
+    private void Camerapermission() {
+        if (ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+                ActivityCompat.requestPermissions(activity,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    public Bitmap rotateImage(Bitmap source, float angle) {
+        Bitmap retVal;
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        retVal = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+
+        return retVal;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap bmp = (Bitmap) data.getExtras().get("data");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+            ivimage.setImageBitmap(bmp);
+
+        } else if (requestCode == Gallary_REQUEST && resultCode == Activity.RESULT_OK) {
+            Uri selectedImageUri = data.getData();
+
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImageUri, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            Bitmap bmp = BitmapFactory.decodeFile(picturePath);
+            ExifInterface ei = null;
+            try {
+                ei = new ExifInterface(picturePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotateImage(bmp, 90);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotateImage(bmp, 180);
+                    break;
+                // etc.
+            }
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+
+            ivimage.setImageBitmap(bmp);
+
+        }
+    }
+
+    void openCamera() {
+        File directory = new File(
+                Environment.getExternalStorageDirectory(), "AttackLab Pics" + "/" + getPackageName());
+        try {
+            SimpleDateFormat sdfPic = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            String currentDateandTime = sdfPic.format(new Date()).replace(" ", "");
+            File imagesFolder = new File(directory.getAbsolutePath());
+            imagesFolder.mkdirs();
+
+            String fname = "IMAGE_" + currentDateandTime + ".jpg";
+            File file = new File(imagesFolder, fname);
+            fileUri = Uri.fromFile(file);
+            Intent cameraIntent = new Intent(
+                    android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
